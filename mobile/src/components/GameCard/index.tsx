d7b4dbeacc2726c6d256cba92dbefc885ad86f2c
João Otávio da Silva <joao.otavio1225@gmail.com>
2022-09-16 15:52:22 -0300

    Aula 4

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -1,14 +1,16 @@
-import { Text, TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import { TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType, Text } from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
 
 import { styles } from './styles';
-import { THEME } from '../../theme';
+import { THEME } from "../../theme";
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: {
+    ads: number;
+  }
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -17,24 +19,22 @@ interface Props extends TouchableOpacityProps {
 
 export function GameCard({ data, ...rest }: Props) {
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} {...rest}>
       <ImageBackground
         style={styles.cover}
-        source={data.cover}
+        source={{ uri: data.bannerUrl }}
       >
-
         <LinearGradient
           colors={THEME.COLORS.FOOTER}
           style={styles.footer}
         >
           <Text style={styles.name}>
-            {data.name}
+            {data.title}
           </Text>
 
           <Text style={styles.ads}>
-            {data.ads} anúncios
+            {data._count.ads} anúncios
           </Text>
-
         </LinearGradient>
       </ImageBackground>
     </TouchableOpacity>
